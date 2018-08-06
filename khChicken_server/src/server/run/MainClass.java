0d189cf2bf56bf40d0be89abb72c48ff4a2f0d20
BaekSeungJi <tmdwlfk@naver.com>
2018-08-06 21:31:50 +0900

    seungji

--- khChicken_server/src/server/run/MainClass.java
+++ khChicken_server/src/server/run/MainClass.java
@@ -1,12 +1,21 @@
 package server.run;
 
-import server.communicator.SocketControl;
+import server.dao.MenuDao;
+import server.db.DBConnection;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		new SocketControl().serverOpen();
+		/*System.out.println("hello");
+		new SocketControl().serverOpen();*/
+		
+		DBConnection.initConnect();
+		
+		new MenuDao().insert();
+		
+		
+		//new MenuDao().select();
+		
 	}
 
 }
