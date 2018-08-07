53285d16b4306b0a2ce62c1792bf028a4df3fc67
BaekSeungJi <tmdwlfk@naver.com>
2018-08-07 17:41:12 +0900

    seungji

--- khChicken_server/src/server/dao/OrderDao.java
+++ khChicken_server/src/server/dao/OrderDao.java
@@ -44,6 +44,7 @@ public class OrderDao {
 		}
 		
 		
+		
 				/*
 				INSERT INTO EMP(employee_id, first_name, last_name, email, phone_number, hire_date,
                 job_id, salary, commission_pct, manager_id, department_id)
