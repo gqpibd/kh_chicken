a6dddff4f8fa78e8637d0eadf8187b5796006210
BaekSeungJi <tmdwlfk@naver.com>
2018-08-08 20:19:06 +0900

    seungji

--- khChicken_client/src/client/communicator/Communicator.java
+++ khChicken_client/src/client/communicator/Communicator.java
@@ -1,6 +1,10 @@
 package client.communicator;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
@@ -18,7 +22,7 @@ import client.dto.ReviewDto;
 import client.singleton.Singleton;
 import client.view.SaleManageView;
 
-public class Communicator {
+public class Communicator extends Thread{
 	private Socket sock;
 	
 
@@ -64,6 +68,90 @@ public class Communicator {
 	}
 	
 	
+	
+	@Override
+	public void run() {
+		super.run();
+		
+		Singleton s = Singleton.getInstance();
+
+		//while (true) {
+			try {
+				
+				//input. output 스트림 생성, printwriter 생성(번호 받기용)
+				InputStream input = sock.getInputStream();
+				//OutputStream out = sock.getOutputStream();
+				//PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
+
+				//pw.println("Welcome");
+				//pw.flush();
+
+				// receive
+				BufferedReader br = new BufferedReader(new InputStreamReader(input)); // client에서 받은 번호 input
+				int number;
+				number = Integer.parseInt(br.readLine());
+				System.out.println("received number : " + number);
+				
+				
+				ObjectInputStream ois = new ObjectInputStream(input); // dto받기
+				Object obj = ois.readObject();
+				System.out.println(obj.toString());
+				
+				s.ordCtrl.select(obj);
+				
+				
+				
+//				switch (number) {
+//				case 0:	//insert	
+//				case 1:	//select
+//				case 2:	//delete
+//				case 3:	//update
+//					
+//					obj = ois.readObject();
+//					
+//					//어떤 dto 인지 구분
+//					if (obj instanceof MemberDto) {	
+//
+//					} else if (obj instanceof MenuDto) {
+//
+//					} else if (obj instanceof MenuShowDto) {
+//
+//					} else if (obj instanceof OrderedMenuDto) {
+//
+//					} else if (obj instanceof ReviewDto) {
+//
+//					}
+//					break;
+//
+//				case 4: // menu 불러오기
+//					
+//				case 5: // review 불러오기
+//					
+//				case 6: // 전체매출 불러오기
+//					obj = s.ctrlOrder.select();
+//					break;
+//				case 7: // 내 주문내역 불러오기
+//
+//				}
+//
+//				// send (받는건 번호+dto 지만 보내는건 한번만 해도됨)
+//				ObjectOutputStream oos = new ObjectOutputStream(sock.getOutputStream());
+//				oos.writeObject(obj);
+				
+				sleep(100);
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+
 	public Object SendAndReceiveMessage() {
 		
 		return null;
