5018ffdd6f976c5a8a5f855dd80db081c2917bda
fnvlektmf <fnvlektmf@naver.com>
2018-08-09 20:24:47 +0900

    ㅇㅇ

--- khChicken_server/src/dto/MenuShowDto.java
+++ khChicken_server/src/dto/MenuShowDto.java
@@ -7,39 +7,52 @@ public class MenuShowDto extends MenuDto implements Serializable {
 	
 	private static final long serialVersionUID = 7675418139620414668L;
 	
-	private String image;
-	//private int reviewCount;	best메뉴만들기위해서 해놨었음
+	private String type;
+	private String description;
 	private double avgScore;
-	
+
 	public MenuShowDto() {
 	}
 
-	public MenuShowDto(String image, double avgScore) {
-		super();
-		this.image = image;
+	public MenuShowDto(String name, int price) {
+		super(name, price);
+		this.avgScore = 0;
+	}
+	
+	public MenuShowDto(String name, int price, String type, String description, double avgScore) {
+		super(name, price);
+		this.type = type;
+		this.description = description;
 		this.avgScore = avgScore;
 	}
 
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
-
-
-	public double getAvgScore() {
+	public double getavgScore() {
 		return avgScore;
 	}
 
-	public void setAvgScore(double avgScore) {
+	public void setavgScore(double avgScore) {
 		this.avgScore = avgScore;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	@Override
 	public String toString() {
-		return "MenuShowDto [image=" + image + ", avgScore=" + avgScore + "]";
+		return "MenuShowDto [ menuName =" + super.getMenu_name() + ", price = " + super.getPrice() + ", type=" + type + ", description=" + description + ", avgScore=" + avgScore + "]";
 	}
 	
 	
