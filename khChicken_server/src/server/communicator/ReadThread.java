084db06b6fc15b66decec6a006baf55afe1472fd
상필아 빡공하자 < @DESKTOP-DOPLCRC>
2018-08-10 01:53:18 +0900

    dd

--- khChicken_server/src/server/communicator/ReadThread.java
+++ khChicken_server/src/server/communicator/ReadThread.java
@@ -11,9 +11,8 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.HashMap;
 
-import client.dto.MemberDto;
+import server.dto.MemberDto;
 import server.dto.MenuDto;
 import server.dto.MenuShowDto;
 import server.dto.OrderedMenuDto;
@@ -33,53 +32,35 @@ public class ReadThread extends Thread {
 		super.run();
 		Singleton s = Singleton.getInstance();
 
-		ObjectInputStream ois = null;
-		
+		while (true) {
 			try {
-				while (true) {
-				//input. output 스트림 생성, printwriter 생성(번호 받기용)
+				
+				//input. output �뒪�듃由� �깮�꽦, printwriter �깮�꽦(踰덊샇 諛쏄린�슜)
 				InputStream input = sock.getInputStream();
-				OutputStream ouyput = sock.getOutputStream();
 				OutputStream out = sock.getOutputStream();
-				
-				// receive
-				BufferedReader br = new BufferedReader(new InputStreamReader(input)); // client에서 받은 번호 input
-				int number = Integer.parseInt(br.readLine());
-					
-				PrintWriter pw = new PrintWriter(ouyput);
-				pw.println("1");
+				PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
+
+				pw.println("Welcome");
 				pw.flush();
-				
-				
+				// receive
+				BufferedReader br = new BufferedReader(new InputStreamReader(input)); // client�뿉�꽌 諛쏆� 踰덊샇 input
+
 				Object obj = null;
-					ois = new ObjectInputStream(input); // dto받기
+				int number;
+
+				number = Integer.parseInt(br.readLine());
+				System.out.println("received: " + number);
+				
+				switch (number) {
+				case 0:	//insert	
+				case 1:	//select
+				case 2:	//delete
+				case 3:	//update
+					ObjectInputStream ois = new ObjectInputStream(input); // dto諛쏄린
 					obj = ois.readObject();
-					MemberDto dto = (MemberDto)obj;
 					
-					//어떤 dto 인지 구분
-					if (obj instanceof MemberDto) {
-						dto = (MemberDto)obj;
-						
-						switch (number) {
-						case 0:	//insert
-							System.out.println(dto.getId());
-							s.ctrlMember.insert(dto);
-							break;
-						case 1:	//select
-							boolean join = s.ctrlMember.select(dto);
-							ObjectOutputStream oos = new ObjectOutputStream(ouyput);
-							oos.writeObject(join);
-							oos.flush();
-							
-
-
-							boolean Loging = s.ctrlMember.select_loging(dto);
-							oos = new ObjectOutputStream(out);
-							System.out.println(Loging);
-							oos.writeObject(Loging);
-							oos.flush();
-							break;
-						}
+					//�뼱�뼡 dto �씤吏� 援щ텇
+					if (obj instanceof MemberDto) {	
 
 					} else if (obj instanceof MenuDto) {
 
@@ -90,35 +71,33 @@ public class ReadThread extends Thread {
 					} else if (obj instanceof ReviewDto) {
 
 					}
-					
-				/*case 4: // menu 불러오기
-					
-				case 5: // review 불러오기
-					
-				case 6: // 전체매출 불러오기
-					
-				case 7: // 내 주문내역 불러오기
-*/
+					break;
 
-				// send (받는건 번호+dto 지만 보내는건 한번만 해도됨)
-			//	ObjectOutputStream oos = new ObjectOutputStream(sock.getOutputStream());
-			//	oos.writeObject(obj);
-			//	sleep(100);
+				case 4: // menu 遺덈윭�삤湲�
 					
-				
+				case 5: // review 遺덈윭�삤湲�
+					
+				case 6: // �쟾泥대ℓ異� 遺덈윭�삤湲�
+					
+				case 7: // �궡 二쇰Ц�궡�뿭 遺덈윭�삤湲�
 
 				}
+
+				// send (諛쏅뒗嫄� 踰덊샇+dto 吏�留� 蹂대궡�뒗嫄� �븳踰덈쭔 �빐�룄�맖)
+				ObjectOutputStream oos = new ObjectOutputStream(sock.getOutputStream());
+				oos.writeObject(obj);
+				sleep(100);
+
 			} catch (FileNotFoundException e) {
-			//	e.printStackTrace();
-			} catch (IOException e) {
-			//	e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-			//	e.printStackTrace();
-			} /*catch (InterruptedException e) {
 				e.printStackTrace();
-			}*/ 
-			
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 
-	
+	}
 }
