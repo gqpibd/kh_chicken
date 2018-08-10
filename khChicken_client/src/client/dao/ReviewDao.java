103b3d8a7f2e9641f25d308b656347e08b23d9ae
상필아 빡공하자 < @DESKTOP-DOPLCRC>
2018-08-10 13:26:20 +0900

    dd

--- khChicken_client/src/client/dao/ReviewDao.java
+++ khChicken_client/src/client/dao/ReviewDao.java
@@ -1,28 +1,69 @@
 package client.dao;
 
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import client.dto.ReviewDto;
 
 public class ReviewDao {
 	
 	List<ReviewDto> rList = new ArrayList<ReviewDto>();
-	Socket sock
+	Socket sock;
 	
 	public ReviewDao() {
 	}
 	
-	public void SockReview(Socket sock) {
+	public void SockDao(Socket sock) {
 		this.sock = sock;
+		
 	}
 	
 	public void insert() {
 		
 	}
 	
-	public void select() {
+	public List<ReviewDto> select() {
+		
+		List<ReviewDto> list = new ArrayList<>();
+		ReviewDto dto = new ReviewDto();
+		ObjectOutputStream oos;
+		try {
+			oos= new ObjectOutputStream(sock.getOutputStream());
+			oos.writeInt(1);
+			oos.flush();
+			oos= new ObjectOutputStream(sock.getOutputStream());
+			oos.writeObject(dto);
+			oos.flush();
+			System.out.println(11);
+			
+			ObjectInputStream ois;
+			ois = new ObjectInputStream(sock.getInputStream());
+			System.out.println(ois);
+			list =(List<ReviewDto>)ois.readObject();
+			System.out.println(22);
+			
+		
+		
+		
+		
+		} catch (ClassNotFoundException e) {
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return list;
+		
+		
 		
 	}
 	
--- khChicken_client/src/client/view/Window_Review.java
+++ khChicken_client/src/client/view/Window_Review.java
@@ -1,9 +1,11 @@
 package client.view;
 
 import java.awt.BorderLayout;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -11,6 +13,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.security.Signature;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
@@ -24,21 +29,23 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-import com.sun.xml.internal.ws.api.server.Container;
+import client.dto.ReviewDto;
+import client.singleton.Singleton;
+
 
 
 public class Window_Review extends JFrame implements ActionListener,  MouseListener {
 	
 	
-		Image Image_Fired; // 후라이드치킨
-		ImageIcon Imageicon_Fired; // 후라이드치킨 사이즈맞춘거
-		JLabel JLabe_Fired; // 후라이드 치킨 간판
+		Image Image_Fired;
+		ImageIcon Imageicon_Fired;
+		JLabel JLabe_Fired; 
 		
 		
-		ImageIcon ImageIcon_LogoFired; //후라이드 로그 
-		JLabel JLabe_LogoFired; // 후라이드로그
+		ImageIcon ImageIcon_LogoFired; 
+		JLabel JLabe_LogoFired; 
 		
-		JLabel JLabe_explanation; // 음식소개글
+		JLabel JLabe_explanation; 
 		
 		JButton JBut_ReviewRead;
 		JPanel container;
@@ -48,16 +55,45 @@ public class Window_Review extends JFrame implements ActionListener,  MouseListe
 		JScrollPane JScroll_Review;
 		JButton JBut_reviewInput;
 		
+		List<ReviewDto> list =new ArrayList<ReviewDto>();
+		
+		JPanel jp[];
+		JLabel jl[];
+		JPanel jpp;
+		
 	public Window_Review() {
 
-		super("리뷰창");
+		super("리뷰");
 		setLayout(null);
 		
-			
+		jpp = new JPanel();
 		
+		Singleton single = Singleton.getInstance();
+		list = single.revCtrl.select();
+		jp = new JPanel[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(i);
+			jp = new JPanel[i+1];
+			jl =new JLabel[i+1];
+			jp[i] = new JPanel();
+			
+			jp[i].setLayout(new FlowLayout());
+			jp[i].setBounds(100, 250*i, 200, 200);
+			//jl[i] = new JLabel(list.get(i).getUserId());
+			jl[i] = new JLabel(list.get(i).getReview());
+			System.out.println(jl[i].getText());
+			jp[i].add(jl[i]);
+			//JTextArea jjta = new JTextArea(list.get(i).getReview());
+			//jjta.setBounds(0, 50, 400, 400);
+			//add(jjta);
+			//jp[i].setBackground(Color.WHITE);
+			jpp.add(jp[i]);
+		}
+		jpp.setBounds(100, 250, 200, 200);
+		add(jpp);
 			Toolkit Toolk_Fired = Toolkit.getDefaultToolkit();
 			Image_Fired = Toolk_Fired.getImage("Fried.png");
-			Image_Fired = Image_Fired.getScaledInstance(261, 189, 200); // 사진 사이즈 조절
+			Image_Fired = Image_Fired.getScaledInstance(261, 189, 200); // ?���? ?��?���? 조절
 			Imageicon_Fired = new ImageIcon(Image_Fired);
 			JLabe_Fired = new JLabel(Imageicon_Fired);
 			JLabe_Fired.setBounds(88, 100, 261, 191);
@@ -68,7 +104,7 @@ public class Window_Review extends JFrame implements ActionListener,  MouseListe
 			JLabe_LogoFired.setBounds(88, 10, 261, 67);
 		//	add(JLabe_LogoFired);
 			
-		JLabe_explanation = new JLabel(/*"<html>이것은 너무나도 맛있는 치킨입니다<br/>정말 맛있는 치킨입니다 <br/> 레알로 맛있는 치킨입니다. <br/> 진짜 개맛있는 치킨입니다. <br/> 한번먹으면 빠져나올수 없는 치킨입니다. <br/> 그것은 바로 후라이드 치킨입니다</html>"*/);
+		JLabe_explanation = new JLabel(/*"<html>?��것�? ?��무나?�� 맛있?�� 치킨?��?��?��<br/>?���? 맛있?�� 치킨?��?��?�� <br/> ?��?���? 맛있?�� 치킨?��?��?��. <br/> 진짜 개맛?��?�� 치킨?��?��?��. <br/> ?��번먹?���? 빠져?��?��?�� ?��?�� 치킨?��?��?��. <br/> 그것?? 바로 ?��?��?��?�� 치킨?��?��?��</html>"*/);
 		JLabe_explanation.setBounds(98, 301, 226, 126);
 		//JLabe_explanation.setBackground(Color.white);
 		JLabe_explanation.setOpaque(true);
@@ -76,8 +112,8 @@ public class Window_Review extends JFrame implements ActionListener,  MouseListe
 		
 		JBut_ReviewRead = new JButton("리뷰");
 		JBut_ReviewRead.addActionListener(this);
-		JBut_ReviewRead.setBounds(200, 550, 100, 50);
-		//add(JBut_ReviewRead);
+		JBut_ReviewRead.setBounds(0, 0, 100, 50);
+		add(JBut_ReviewRead);
 		
 		
 		
@@ -100,9 +136,9 @@ public class Window_Review extends JFrame implements ActionListener,  MouseListe
 			Jpanel_Review[i] = new JPanel();
 			Jpanel_Review[i].setLayout(null);
 			Jpanel_Review[i].setBounds(0, 60 * i, 100, 50);
-			ja[i] = new JLabel(i+"하하하");
+			ja[i] = new JLabel(i+"");
 			ja[i].setBounds(0, -10, 500, 50);
-			jta[i] = new JTextArea("안녕하세요"+i);
+			jta[i] = new JTextArea(""+i);
 			jta[i].setBounds(0, 27, 100, 52);
 			//jta[i].setEditable(false);
 			Jpanel_Review[i].add(ja[i]);
@@ -123,30 +159,9 @@ public class Window_Review extends JFrame implements ActionListener,  MouseListe
 		container.add(new JScrollPane(list),BorderLayout.CENTER);
 		JScrollPane JSc =  new JScrollPane(container);
 		JSc.setBounds(0, 0, 400, 400);
-		add(JSc);
-		
 		//add(JSc);
 		
-		
-		
-	//	scrPane = new JScrollPane(container);
-//		container.setBounds(0, 0, 400, 500);
-	//	add(container);
-		
-	//	JPanel JPane_Review = new JPanel();
-	//	JPane_Review.setLayout(new FlowLayout());
-	//	JTextA_Review =new JTextArea();
-	//	JBut_reviewInput = new JButton("입력");
-	//	JBut_reviewInput.addActionListener(this);
-	//	container.add(JTextA_Review);
-	//	container.add(JBut_reviewInput);
-	//	JPane_Review.setBounds(200, 200, 100, 100);
-	//	add(scrPane);
-		
-		
-		//scrPane.setVisible(false);
-		
-		//getContentPane().setBackground(Color.WHITE);
+
 		setBounds(100, 100, 450, 589);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -155,24 +170,29 @@ public class Window_Review extends JFrame implements ActionListener,  MouseListe
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		//Singleton single = Singleton.getInstance();
 		Object obj = e.getSource();
 		if(obj == JBut_ReviewRead) {
-		if(JBut_ReviewRead.getText().equals("닫기")) {
+		
+			if(JBut_ReviewRead.getText().equals("닫기")) {
 			JBut_ReviewRead.setText("리뷰");
+			
+			
+			jpp.setVisible(false);
 			//scrPane.setVisible(false);
 			//repaint();
 			//dispose();
 			//new Window_Review();
 		}else if(JBut_ReviewRead.getText().equals("리뷰")) {
 			JBut_ReviewRead.setText("닫기");
-			//scrPane.setVisible(true);
+			for(int i = 0; i < list.size(); i++) {
+				jpp.setVisible(true);
 		}
 		
-		}
 	}
+		}
 	
-
+	}
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
