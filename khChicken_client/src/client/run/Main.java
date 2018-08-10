d14bf9cb0bf9580403223bc7f8cfca8ba0eb1954
fnvlektmf <fnvlektmf@naver.com>
2018-08-10 16:27:15 +0900

    d

--- khChicken_client/src/client/run/Main.java
+++ khChicken_client/src/client/run/Main.java
@@ -1,12 +1,13 @@
 package client.run;
 
 import client.communicator.Communicator;
+import client.view.mainView;
 
 public class Main {
 
 	public static void main(String[] args) {
 		new Communicator().makeConnection();
-		//kkkkk
+		new mainView();
 		
 	}
 
