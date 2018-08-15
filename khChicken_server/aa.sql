31b2d58b4b34209ad8c03ac5342ca6a5d1bac486
j <j@DESKTOP-NSVS9CC>
2018-08-15 16:36:21 +0900

    ccm

--- khChicken_server/aa.sql
+++ khChicken_server/aa.sql
@@ -1,1 +1,6 @@
 select * from order_detail;
+
+UPDATE order_detail  
+SET REVIEW = '맛있어요', SCORE = 5  
+WHERE ID = 'dohy' AND MENU_NAME = '양념 치킨' AND ORDER_DATE =TO_DATE(sysdate - TO_DATE(ORDER_DATE,'YYYY-MM-DD HH:MI:SS') < 2;
+
